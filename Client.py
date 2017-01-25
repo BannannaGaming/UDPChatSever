8e7070cff053efb2500456333bbee292d2f95676
Simon <simjenner3@gmail.com>
2017-01-25 20:19:19 +0000

    Fixed ur code m9

--- Client.py
+++ Client.py
@@ -17,9 +17,6 @@ def receving(name, sock):
         finally:
             thread_lock.release()
 
-host = '127.0.0.1'
-port = 0
-
 server = ('127.0.0.1', 5000)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
